932daf250d27f99393ba34cbae8656984b93204c
Vladimir <Vladimir@VLADIMIR-PC>
2018-09-29 18:32:49 +0300

    Init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Template: Vue + MaterializeCSS + Jest
+
+> This is an example project includes:
+
+* Template: [Vue webpack-simple](https://github.com/vuejs-templates/webpack-simple)
+* CSS framework: [MaterializeCSS](https://materializecss.com/)
+* Unit tests: [Jest](https://jestjs.io/)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at http://localhost:8080.
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="container">
+    <h1>Vue + MaterializeCSS</h1>
+    <ul>
+      <li><a href="https://vuejs.org/">Vue.js docs</a></li>
+      <li><a href="https://materializecss.com/">MaterializeCSS docs</a></li>
+      <li><a href="https://github.com/vuejs-templates/webpack-simple">Webpack-simple template</a></li>
+      <li><a href="https://jestjs.io/">Jest docs</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Vue + MaterializeCSS'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
